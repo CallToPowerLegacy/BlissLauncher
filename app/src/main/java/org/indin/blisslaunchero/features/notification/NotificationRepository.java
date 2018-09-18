f93ee6ad968297bb9de6a845397942d6b5de8652
Amit Kumar <fbpass.res14@gmail.com>
2018-09-19 01:34:23 +0530

    Prevent notification badges for ongoing and foreground notifications

--- app/src/main/java/org/indin/blisslaunchero/features/notification/NotificationRepository.java
+++ app/src/main/java/org/indin/blisslaunchero/features/notification/NotificationRepository.java
@@ -1,14 +1,15 @@
 package org.indin.blisslaunchero.features.notification;
 
+import android.app.Notification;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import com.jakewharton.rxrelay2.BehaviorRelay;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import com.jakewharton.rxrelay2.BehaviorRelay;
-
-import android.service.notification.StatusBarNotification;
-import android.util.Log;
 
 /**
  * Created by Amit Kumar
@@ -37,6 +38,13 @@ public class NotificationRepository {
         Log.d(TAG, "updateNotification() called with: list = [" + list.size() + "]");
         Set<String> notificationSet = new HashSet<>();
         for (StatusBarNotification statusBarNotification : list) {
+            Notification notification = statusBarNotification.getNotification();
+            if ((notification.flags & Notification.FLAG_ONGOING_EVENT)
+                    == Notification.FLAG_ONGOING_EVENT
+                    || (notification.flags & Notification.FLAG_FOREGROUND_SERVICE)
+                    == Notification.FLAG_FOREGROUND_SERVICE) {
+                continue;
+            }
             notificationSet.add(statusBarNotification.getPackageName());
         }
         this.notificationRelay.accept(notificationSet);
