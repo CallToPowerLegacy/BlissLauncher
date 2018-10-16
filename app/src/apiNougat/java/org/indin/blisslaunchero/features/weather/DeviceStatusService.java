39d6f319ecfa6bab2e8cb45627021395ee154799
Amit Kumar <fbpass.res14@gmail.com>
2018-10-16 23:36:42 +0530

    Add oreo and nougat flavours for maintaining different source for various parts

---
+++ app/src/apiNougat/java/org/indin/blisslaunchero/features/weather/DeviceStatusService.java
@@ -0,0 +1,72 @@
+package org.indin.blisslaunchero.features.weather;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.IBinder;
+import android.util.Log;
+
+import org.indin.blisslaunchero.framework.utils.Constants;
+
+public class DeviceStatusService extends Service {
+
+    private static final String TAG = DeviceStatusService.class.getSimpleName();
+    private static final boolean D = Constants.DEBUG;
+
+    private BroadcastReceiver mDeviceStatusListenerReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            // Network connection has changed, make sure the weather update service knows about it
+            if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
+                boolean hasConnection = !intent.getBooleanExtra(
+                        ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+
+                if (D) Log.d(TAG, "Got connectivity change, has connection: " + hasConnection);
+
+                Intent i = new Intent(context, WeatherUpdateService.class);
+                if (hasConnection) {
+                    context.startService(i);
+                } else {
+                    context.stopService(i);
+                }
+            } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+                if (D) Log.d(TAG, "onDisplayOff: Cancel pending update");
+                WeatherUpdateService.cancelUpdates(context);
+            } else if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                if (D) Log.d(TAG, "onDisplayOn: Reschedule update");
+                WeatherUpdateService.scheduleNextUpdate(context, false);
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        IntentFilter deviceStatusFilter = new IntentFilter();
+        deviceStatusFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        deviceStatusFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        deviceStatusFilter.addAction(Intent.ACTION_SCREEN_ON);
+        registerReceiver(mDeviceStatusListenerReceiver, deviceStatusFilter);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (D) Log.d(TAG, "Stopping service");
+        unregisterReceiver(mDeviceStatusListenerReceiver);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (D) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ app/src/apiNougat/java/org/indin/blisslaunchero/features/weather/ForecastBuilder.java
@@ -0,0 +1,120 @@
+package org.indin.blisslaunchero.features.weather;
+
+import static cyanogenmod.providers.WeatherContract.WeatherColumns.TempUnit.CELSIUS;
+import static cyanogenmod.providers.WeatherContract.WeatherColumns.TempUnit.FAHRENHEIT;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.indin.blisslaunchero.R;
+import org.indin.blisslaunchero.framework.Preferences;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import cyanogenmod.weather.WeatherInfo;
+import cyanogenmod.weather.util.WeatherUtils;
+
+public class ForecastBuilder {
+
+    private static final String TAG = "ForecastBuilder";
+
+    /**
+     * This method is used to build the small, horizontal forecasts panel
+     * @param context Context to be used
+     * @param smallPanel a horizontal linearlayout that will contain the forecasts
+     * @param w the Weather info object that contains the forecast data
+     */
+    @SuppressLint("InflateParams")
+    public static void buildSmallPanel(Context context, LinearLayout smallPanel, WeatherInfo w) {
+        if (smallPanel == null) {
+            Log.d(TAG, "Invalid view passed");
+            return;
+        }
+
+        // Get things ready
+        LayoutInflater inflater
+                = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        int color = Preferences.weatherFontColor(context);
+        final boolean useMetric = Preferences.useMetricUnits(context);
+
+        smallPanel.removeAllViews();
+        List<WeatherInfo.DayForecast> forecasts = w.getForecasts();
+        if (forecasts.size() <= 1) {
+            smallPanel.setVisibility(View.GONE);
+            return;
+        }
+
+        TimeZone MyTimezone = TimeZone.getDefault();
+        Calendar calendar = new GregorianCalendar(MyTimezone);
+        int weatherTempUnit = w.getTemperatureUnit();
+        int numForecasts = forecasts.size();
+        int itemSidePadding = context.getResources().getDimensionPixelSize(
+                R.dimen.forecast_item_padding_side);
+
+        // Iterate through the Forecasts
+        for (int count = 0; count < numForecasts; count ++) {
+            WeatherInfo.DayForecast d = forecasts.get(count);
+
+            // Load the views
+            assert inflater != null;
+            View forecastItem = inflater.inflate(R.layout.item_weather_forecast, null);
+
+            // The day of the week
+            TextView day = forecastItem.findViewById(R.id.forecast_day);
+            day.setText(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT,
+                    Locale.getDefault()));
+            calendar.roll(Calendar.DAY_OF_WEEK, true);
+
+            // Weather Image
+            ImageView image = forecastItem.findViewById(R.id.weather_image);
+            String iconsSet = Preferences.getWeatherIconSet(context);
+            final int resId = WeatherIconUtils.getWeatherIconResource(context, iconsSet,
+                    d.getConditionCode());
+            if (resId != 0) {
+                image.setImageResource(resId);
+            } else {
+                image.setImageBitmap(WeatherIconUtils.getWeatherIconBitmap(context, iconsSet,
+                        color, d.getConditionCode()));
+            }
+
+            // Temperatures
+            double lowTemp = d.getLow();
+            double highTemp = d.getHigh();
+            int tempUnit = weatherTempUnit;
+            if (weatherTempUnit == FAHRENHEIT && useMetric) {
+                lowTemp = cyanogenmod.weather.util.WeatherUtils.fahrenheitToCelsius(lowTemp);
+                highTemp = cyanogenmod.weather.util.WeatherUtils.fahrenheitToCelsius(highTemp);
+                tempUnit = CELSIUS;
+            } else if (weatherTempUnit == CELSIUS && !useMetric) {
+                lowTemp = cyanogenmod.weather.util.WeatherUtils.celsiusToFahrenheit(lowTemp);
+                highTemp = cyanogenmod.weather.util.WeatherUtils.celsiusToFahrenheit(highTemp);
+                tempUnit = FAHRENHEIT;
+            }
+            String dayLow = cyanogenmod.weather.util.WeatherUtils.formatTemperature(lowTemp, tempUnit);
+            String dayHigh = WeatherUtils.formatTemperature(highTemp, tempUnit);
+            TextView temps = forecastItem.findViewById(R.id.weather_temps);
+            temps.setText(String.format("%s\n%s", dayLow, dayHigh));
+
+            // Add the view
+            smallPanel.addView(forecastItem,
+                    new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1));
+
+            // Add a divider to the right for all but the last view
+            if (count < numForecasts - 1) {
+                View divider = new View(context);
+                smallPanel.addView(divider, new LinearLayout.LayoutParams(
+                        itemSidePadding, LinearLayout.LayoutParams.MATCH_PARENT));
+            }
+        }
+    }
+}
